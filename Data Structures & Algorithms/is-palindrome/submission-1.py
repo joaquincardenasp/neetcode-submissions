1f6f1f21408365dfec0cb6a7ffb360dbe396d9f4
Joaquin Cardenas <jicardenas@miuandes.cl>
2026-06-04 00:00:54 -0400

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_text = re.sub(r'[^a-zA-Z0-9]', '', s).lower()
+        left, right = 0, len(clean_text) - 1
+        while left < right:
+            if clean_text[left] != clean_text[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
+
+# using regex
