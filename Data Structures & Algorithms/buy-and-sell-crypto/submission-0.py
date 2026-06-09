954ad306bc14f99c7988b5feb14b34955a434670
Joaquin Cardenas <jicardenas@miuandes.cl>
2026-06-09 00:07:50 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        diff = 0
+        for i in range(len(prices)):
+            for j in range(1,len(prices)):
+                if (prices[j]-prices[i]) > diff and j>i :
+                    print(i,j)
+                    diff = prices[j] - prices[i]
+        return diff
+            
